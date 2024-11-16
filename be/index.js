b17f5b0204e06385c0fa57c37afb7a67c4ebe2ad
Hwanhee Chae <hh@hwan.me>
2024-11-16 15:06:45 +0900

    add cors

--- be/index.js
+++ be/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(require("./routes/mediaRoute"));
 
